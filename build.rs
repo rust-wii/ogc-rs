2e60e451190a95b1c17a1bf0109d4b3d2394c927
Fenrir <fenrirwolf@gmail.com>
2019-06-05 19:55:16 -0600

    Use env var to specify library search path

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=/opt/devkitpro/libogc/lib/wii");
+    let dkp_path = std::env::var("DEVKITPRO").unwrap();
+
+    println!("cargo:rustc-link-search=native={}/libogc/lib/wii", dkp_path);
     println!("cargo:rustc-link-lib=static=ogc");
 }
