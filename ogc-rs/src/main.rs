6134d9f208eb35fe72cf2f7ad86eb3f5543cbfa4
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2019-06-12 19:46:02 +0100

    Structure ogc-rs main.rs

--- ogc-rs/src/main.rs
+++ ogc-rs/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-}
+//! # ogc-rs
+//!
+//! ``ogc-rs`` is a safe, idiomatic wrapper around ``ogc-sys``.
+//!
+//! ``ogc-rs`` provides many features from libogc such as:
+//!
+//! * ``network``: Provides TCP networking for the Wii.
+//! * ``audio``: Provides functions for audio on the Wii.
+//! * ``fs``: Provides functions for manipulating the filesystem on the Wii.
+//! * ``console``: Provides console functions for the Wii.
+//! * ``input``: Provides an interface for reading input from devices on the Wii.
+//! * ``video``: Provides functions for video output on the Wii.
+//! * ``gx``: Provides an opengl-like interface for rendering on the Wii.
+//!
+//! Features such as ``network``, ``fs`` and ``console`` have been merged into ``std`` to extend functionality.
+//! However it is still possible to use these features if you require certain functionality missing from ``std``.
+
+#![crate_type = "rlib"]
+#![crate_name = "ogc"]
+
+/// Custom Error Type for functions in this library.
+pub mod error;
