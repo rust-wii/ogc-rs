386bc34061ff581641e74ce3ee9cb2bc43c06ad3
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2019-08-18 16:50:24 +0100

    Add system back into README

--- ogc-rs/src/lib.rs
+++ ogc-rs/src/lib.rs
@@ -7,6 +7,7 @@
 //! * ``network``: Provides TCP networking for the Wii.
 //! * ``audio``: Provides functions for audio on the Wii.
 //! * ``fs``: Provides functions for manipulating the filesystem on the Wii.
+//! * ``system``: Provides OS functions for the Wii.
 //! * ``console``: Provides console functions for the Wii.
 //! * ``input``: Provides an interface for reading input from devices on the Wii.
 //! * ``video``: Provides functions for video output on the Wii.
