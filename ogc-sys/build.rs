bbc173da133cf40da75143e4de12df7cea57e293
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-09-04 16:30:07 +0100

    Force build to link against libc and sysbase

--- ogc-sys/build.rs
+++ ogc-sys/build.rs
@@ -1,6 +1,12 @@
 fn main() {
     let dkp_path = std::env::var("DEVKITPRO").unwrap();
 
+    println!(
+        "cargo:rustc-link-search=native={}/devkitPPC/powerpc-eabi/lib",
+        dkp_path
+    );
     println!("cargo:rustc-link-search=native={}/libogc/lib/wii", dkp_path);
+    println!("cargo:rustc-link-lib=static=c");
+    println!("cargo:rustc-link-lib=static=sysbase");
     println!("cargo:rustc-link-lib=static=ogc");
 }
