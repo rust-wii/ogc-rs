e652aa6407f2b288d5096b1e2792281b132437a8
ProfElements <profelements1@gmail.com>
2021-09-26 20:09:01 -0500

    :recycle: refactor(examples): Make examples build again!

--- examples/embedded-graphics-wii/src/display.rs
+++ examples/embedded-graphics-wii/src/display.rs
@@ -10,9 +10,8 @@ use embedded_graphics::{
 use ogc_rs::{
     ffi::{
         Mtx, GX_CLR_RGBA, GX_COLOR0A0, GX_DIRECT, GX_F32, GX_GM_1_0, GX_MAX_Z24, GX_NONE,
-        GX_ORTHOGRAPHIC, GX_PASSCLR, GX_PF_RGB8_Z24, GX_PNMTX0, GX_POS_XYZ, GX_RGBA8, GX_TEVSTAGE0,
-        GX_TEXCOORD0, GX_TEXMAP0, GX_TEX_ST, GX_VA_CLR0, GX_VA_POS, GX_VA_TEX0, GX_VTXFMT0,
-        GX_ZC_LINEAR,
+        GX_PASSCLR, GX_PF_RGB8_Z24, GX_PNMTX0, GX_POS_XYZ, GX_RGBA8, GX_TEVSTAGE0, GX_TEXCOORD0,
+        GX_TEXMAP0, GX_TEX_ST, GX_VA_CLR0, GX_VA_POS, GX_VA_TEX0, GX_VTXFMT0, GX_ZC_LINEAR,
     },
     mem_cached_to_uncached,
     prelude::*,
@@ -119,7 +118,7 @@ impl Display {
             0.0,
             1000.0,
         );
-        Gx::load_projection_mtx(&mut perspective, GX_ORTHOGRAPHIC as _);
+        Gx::load_projection_mtx(&mut perspective, ProjectionType::Orthographic);
 
         Gx::set_viewport(
             0.0,
