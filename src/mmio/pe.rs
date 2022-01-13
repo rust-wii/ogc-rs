bbf9730834be1b4eac9b18a15b319615ca6dde7a
ProfElements <profelements1@gmail.com>
2022-01-13 10:26:37 -0600

    chore(MMIO): :art: cargo format mmio

--- src/mmio/pe.rs
+++ src/mmio/pe.rs
@@ -1,12 +1,14 @@
-use voladdress::{VolAddress, Safe, Unsafe};
+use voladdress::{Safe, Unsafe, VolAddress};
 
 #[repr(transparent)]
 pub struct ZConfigControl(u16);
-pub const Z_CONFIG: VolAddress<ZConfigControl, Safe, Safe> = unsafe { VolAddress::new(0xCC00_1000) };
+pub const Z_CONFIG: VolAddress<ZConfigControl, Safe, Safe> =
+    unsafe { VolAddress::new(0xCC00_1000) };
 
 #[repr(transparent)]
-pub struct AlphaConfigControl(u16); 
-pub const ALPHA_CONFIG: VolAddress<AlphaConfigControl, Safe, Safe> = unsafe { VolAddress::new(0xCC00_1002) };
+pub struct AlphaConfigControl(u16);
+pub const ALPHA_CONFIG: VolAddress<AlphaConfigControl, Safe, Safe> =
+    unsafe { VolAddress::new(0xCC00_1002) };
 
 #[repr(transparent)]
 pub struct DestAlpha(u16);
@@ -22,6 +24,7 @@ pub const ALPHA_READ: VolAddress<AlphaRead, Safe, Safe> = unsafe { VolAddress::n
 
 #[repr(transparent)]
 pub struct InterruptStatusControl(u16);
-pub const INTERRUPT_STATUS_REGISTER: VolAddress<InterruptStatusControl, Unsafe, Unsafe> = unsafe { VolAddress::new(0xCC00_100A) };
+pub const INTERRUPT_STATUS_REGISTER: VolAddress<InterruptStatusControl, Unsafe, Unsafe> =
+    unsafe { VolAddress::new(0xCC00_100A) };
 
 pub const PE_TOKEN: VolAddress<u16, Safe, Safe> = unsafe { VolAddress::new(0xCC00_100E) };
