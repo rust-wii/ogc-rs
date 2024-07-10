9a1e54f96b83939c9dc57e8e35f172688bbe910d
ProfElements <profelements1@gmail.com>
2024-07-10 18:27:45 -0500

    :art: chore(gctex): Remove unneeded extra stuff from source

--- examples/texture-tri/vendor/gctex/src/bindings.cpp
+++
@@ -1,8 +0,0 @@
-#include "ImagePlatform.hpp"
-
-extern "C" {
-void impl_rii_decode(u8* dst, const u8* src, u32 width, u32 height,
-                     u32 texformat, const u8* tlut, u32 tlutformat) {
-  librii::image::decode(dst, src, width, height, texformat, tlut, tlutformat);
-}
-}
