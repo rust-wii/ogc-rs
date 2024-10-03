3a38bba1b228e3a191bc076256ac02ad0f700114
ProfElements <profelements1@gmail.com>
2024-10-03 12:35:07 -0500

    Make sure the allocation is not null using NonNull

--- src/utils.rs
+++ src/utils.rs
@@ -78,6 +78,11 @@ pub fn alloc_aligned_buffer(buffer: &[u8]) -> Vec<u8> {
     let mut align_buf = unsafe {
         let ptr = alloc::alloc::alloc_zeroed(Layout::from_size_align(size, 32).unwrap());
         Vec::from_raw_parts(ptr, 0, size)
+        let ptr = NonNull::new(alloc::alloc::alloc_zeroed(
+            Layout::from_size_align(size, 32).unwrap(),
+        ))
+        .unwrap();
+        Vec::from_raw_parts(ptr.as_ptr(), 0, size)
     };
     for byte in buffer {
         align_buf.push(*byte);
