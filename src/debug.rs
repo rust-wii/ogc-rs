364daf7e560d85b22dba24e904d268dffcd485c0
1011X <1851619+1011X@users.noreply.github.com>
2025-05-08 04:22:27 -0400

    add bindings and docs to debug module

--- src/debug.rs
+++ src/debug.rs
@@ -4,18 +4,29 @@
 
 use crate::ffi;
 
+/// Default EXI channel. channel can be 0 or 1. Note: Used for device type USBGecko
+pub const DEF_EXICHAN: i32 = ffi::GDBSTUB_DEF_CHANNEL as _;
+
+/// Default TCP port. Note: Used for device type TCP
+pub const DEF_TCPPORT: i32 = ffi::GDBSTUB_DEF_TCPPORT as _;
+
 /// Enum for gdb stub types.
 #[derive(Debug, Eq, PartialEq)]
 #[repr(i32)]
 pub enum GDBStubDevice {
-    Usb = 0,
-    Tcp = 1,
+    /// device type: USBGecko
+    Usb = ffi::GDBSTUB_DEVICE_USB as _,
+    /// device type: BBA-TCP
+    Tcp = ffi::GDBSTUB_DEVICE_TCP as _,
 }
 
 /// Performs the initialization of the debug stub.
+///
+/// * `device_type`: type of device to use. can be either USB or TCP.
+/// * `channel_port`: depending on the used device this can be either the EXI channel or the TCP port.
 pub fn debug_init(device_type: GDBStubDevice, channel_port: i32) {
     unsafe {
-        ffi::DEBUG_Init(device_type as i32, channel_port);
+        ffi::DEBUG_Init(device_type as _, channel_port);
     }
 }
 
