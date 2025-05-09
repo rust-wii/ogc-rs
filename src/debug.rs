75ba3512c64753672d966eaee48ec5d659dede38
1011X <1851619+1011X@users.noreply.github.com>
2025-05-09 14:29:01 -0400

    apply feedback and improve docs for debug.rs

--- src/debug.rs
+++ src/debug.rs
@@ -4,36 +4,42 @@
 
 use crate::ffi;
 
-/// Default EXI channel. channel can be 0 or 1. Note: Used for device type USBGecko
-pub const DEF_EXICHAN: i32 = ffi::GDBSTUB_DEF_CHANNEL as _;
+/// Default EXI channel for use in [`debug_init()`]. Channel can be 0 or 1.
+/// 
+/// **Note**: Used for device type USBGecko
+pub const DEF_EXICHAN: u32 = ffi::GDBSTUB_DEF_CHANNEL;
 
-/// Default TCP port. Note: Used for device type TCP
-pub const DEF_TCPPORT: i32 = ffi::GDBSTUB_DEF_TCPPORT as _;
+/// Default TCP port for use in [`debug_init()`].
+/// 
+/// **Note**: Used for device type TCP.
+pub const DEF_TCPPORT: u32 = ffi::GDBSTUB_DEF_TCPPORT;
 
 /// Enum for gdb stub types.
 #[derive(Debug, Eq, PartialEq)]
-#[repr(i32)]
+#[repr(u32)]
 pub enum GDBStubDevice {
     /// device type: USBGecko
-    Usb = ffi::GDBSTUB_DEVICE_USB as _,
+    Usb = ffi::GDBSTUB_DEVICE_USB,
     /// device type: BBA-TCP
-    Tcp = ffi::GDBSTUB_DEVICE_TCP as _,
+    Tcp = ffi::GDBSTUB_DEVICE_TCP,
 }
 
 /// Performs the initialization of the debug stub.
 ///
 /// * `device_type`: type of device to use. can be either USB or TCP.
-/// * `channel_port`: depending on the used device this can be either the EXI channel or the TCP port.
-pub fn debug_init(device_type: GDBStubDevice, channel_port: i32) {
+/// * `channel_port`: depending on the used device this can be either the EXI
+///   channel or the TCP port.
+pub fn debug_init(device_type: GDBStubDevice, channel_port: u32) {
     unsafe {
-        ffi::DEBUG_Init(device_type as _, channel_port);
+        ffi::DEBUG_Init(device_type as _, channel_port as i32);
     }
 }
 
 /// Stub function to insert the hardware break instruction.
 ///
 /// This function is used to enter the debug stub and to connect with the host.
-/// The developer is free to insert this function at any position in project's source code.
+/// The developer is free to insert this function at any position in project's
+/// source code.
 pub fn insert_break() {
     unsafe {
         ffi::_break();
