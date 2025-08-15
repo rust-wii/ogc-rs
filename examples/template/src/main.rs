acf0148bcdbdefa668a459de9e4ebad3c3edda5e
ProfElements <profelements1@gmail.com>
2025-08-15 00:56:46 -0500

    :bug: fix(examples): `no_mangle` and `no_main` all the examples. Remove #![feature(start)]. Works on dolphin

--- examples/template/src/main.rs
+++ examples/template/src/main.rs
@@ -1,13 +1,12 @@
 #![no_std]
-#![feature(start)]
+#![no_main]
 
 extern crate alloc;
-use core::mem::ManuallyDrop;
 
 use ogc_rs::{mp3player::MP3Player, prelude::*};
 
-#[start]
-fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[no_mangle]
+extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     let mp3 = include_bytes!("../mp3.mp3");
 
     let video = Video::init();
